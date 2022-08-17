ed7da99313a4a2b0ed2ddae5b1764ba6f074a429
jhpoelen <jhpoelen@xs4all.nl>
2022-08-17 16:30:02 -0500

    append namespaces; https://github.com/bio-guoda/preston/issues/168

--- make.sh
+++ make.sh
@@ -12,10 +12,14 @@
 source env.sh
 
 PROV_SHA256=$(echo "$PROV_ID" | grep -o -P '[a-f0-9]{64}$')
-PROV_VERSIONS="${PROV_SHA256}-versions.nq"
+PROV_VERSIONS="content.tsv"
 # A. create narrow dwca datasets
 # 1. pick a preston snapshot 
 
+
+function append_namespace {
+  sed "s+$+\t${PROV_ID}+g"
+}
 
 function generate_versions {
   preston cat --data-dir "${DATA_DIR}" "${PROV_ID}"\
@@ -23,31 +27,33 @@ function generate_versions {
   | grep -v "api\.gbif\.org/v1/dataset"\
   | grep -o -P "hash://sha256/[a-f0-9]{64}"\
   | sort\
-  | uniq
-}
-
-function stream_dwc {
-  cat ${PROV_VERSIONS}\
-  | preston dwc-stream --data-dir "${DATA_DIR}"
+  | uniq\
+  | append_namespace
 }
 
 generate_versions > ${PROV_VERSIONS}
 
-VERSIONS_WITH_STILL_IMAGES="${PROV_SHA256}-with-still-images.txt"
-VERSIONS_WITH_TYPES="${PROV_SHA256}-with-multimedia.txt"
+VERSIONS_WITH_STILL_IMAGES="content-with-still-images.tsv"
+VERSIONS_WITH_TYPES="content-with-multimedia.tsv"
 
 
 echo selecting dwc content with types
 cat "${PROV_VERSIONS}"\
+  | cut -f1\
   | parallel '/bin/bash has-multimedia.sh {1}'\
+  | append_namespace\
   > "${VERSIONS_WITH_TYPES}"
 
 echo selecting dwc content with still images
 cat "${VERSIONS_WITH_TYPES}"\
+  | cut -f1\
   | parallel '/bin/bash has-still-image.sh {1}'\
+  | append_namespace\
   > "${VERSIONS_WITH_STILL_IMAGES}"
 
 echo selecting dwc content with preserved specimen
 cat ${VERSIONS_WITH_STILL_IMAGES}\
+  | cut -f1\
   | parallel '/bin/bash has-specimen.sh {1}'\
-  > ${PROV_SHA256}-with-still-images-and-specimen.txt
+  | append_namespace\
+  > ${PROV_SHA256}-with-still-images-and-specimen.tsv
