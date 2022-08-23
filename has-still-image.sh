f1e4d0f639682c88f6760ca94c7430951227f66b
jhpoelen <jhpoelen@xs4all.nl>
2022-08-23 13:01:00 -0500

    include http://purl.org/dc/elements/1.1/type as documented in https://github.com/jhpoelen/specimen-image-index/issues/1

--- has-still-image.sh
+++ has-still-image.sh
@@ -14,7 +14,7 @@ echo "$1"\
  | sed 's+^+<foo:bar> <http://purl.org/pav/hasVersion> <+g'\
  | sed 's+$+> .+g'\
  | preston dwc-stream --remote "file://${DATA_DIR}"\
- | jq --compact-output 'select(.["http://purl.org/dc/terms/type"])'\
+ | jq --compact-output 'select(.["http://purl.org/dc/terms/type"] or .["http://purl.org/dc/elements/1.1/type"])'\
  | grep StillImage\
  | jq --raw-output '.["http://www.w3.org/ns/prov#wasDerivedFrom"]'\
  | grep -o -P "hash://sha256/[a-f0-9]{64}"\
